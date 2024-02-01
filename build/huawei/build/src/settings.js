3cae27326bfdb539e924d6fb1d84c279e9272f14
sean <564139153@qq.com>
2024-02-01 17:33:18 +0800

    内容修改

---
+++ build/huawei/build/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"huawei",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",server:"",jsList:[],bundleVers:{internal:"070df",main:"34e64"}};
